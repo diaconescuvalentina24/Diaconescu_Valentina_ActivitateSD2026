88fbe17650539f8b68c994c2a68c35208e4d77b2
diaconescuvalentina24 <diaconescuvalentina24@stud.ase.ro>
2026-03-01 15:47:20 +0200

    create vector de carti, copiere carti ieftine/primele n din vector, afisare

--- tema2.c
+++ tema2.c
@@ -35,6 +35,50 @@ void afisareCarte(struct Carte c) {
     }
 }
 
+void afisareVectorCarti(struct Carte* vector, int nrElemente) {
+    for (int i = 0; i < nrElemente; i++) {
+        afisareCarte(vector[i]);
+    }
+}
+
+struct Carte* copiazaPrimeleNCarti(struct Carte* vector, int nrElemente, int nrCopiate) {
+    if (nrCopiate > nrElemente) {
+        nrCopiate = nrElemente;
+    }
+
+    struct Carte* copie = (struct Carte*)malloc(sizeof(struct Carte) * nrCopiate);
+
+    for (int i = 0; i < nrCopiate; i++) {
+        copie[i] = vector[i]; //shallow copy 
+        copie[i].titlu = (char*)malloc(strlen(vector[i].titlu) + 1);
+        strcpy(copie[i].titlu, vector[i].titlu); //deep copy pentru campul char*
+    }
+
+    return copie;
+}
+
+void copiazaCartiIeftine(struct Carte* vector, int nrElemente, float pragMaxim, struct Carte** rezultat, int* dimensiune) {
+    *dimensiune = 0;
+
+    for (int i = 0; i < nrElemente; i++) {
+        if (vector[i].pret <= pragMaxim) {
+            (*dimensiune)++;
+        }
+    }
+
+    *rezultat = (struct Carte*)malloc(sizeof(struct Carte) * (*dimensiune));
+    int k = 0;
+
+    for (int i = 0; i < nrElemente; i++) {
+        if (vector[i].pret <= pragMaxim) {
+            (*rezultat)[k] = vector[i];
+            (*rezultat)[k].titlu = (char*)malloc(strlen(vector[i].titlu) + 1);
+            strcpy((*rezultat)[k].titlu, vector[i].titlu);
+            k++;
+        }
+    }
+}
+
 void modificaPretCarte(struct Carte* c, float pretNou) {
     if (c != NULL && pretNou > 0) {
         c->pret = pretNou;
@@ -48,15 +92,57 @@ void dezalocareCarte(struct Carte* c) {
     }
 }
 
+void dezalocareVectorCarti(struct Carte** vector, int* nrElemente) {
+    if (*vector != NULL) {
+        for (int i = 0; i < *nrElemente; i++) {
+            if ((*vector)[i].titlu != NULL) {
+                free((*vector)[i].titlu);
+            }
+        }
+
+        free(*vector);
+        *vector = NULL;
+        *nrElemente = 0;
+    }
+}
+
 int main() {
-    struct Carte c1 = initializareCarte(1, 320, "Morometii", 45.5f, '1');
+    /*struct Carte c1 = initializareCarte(1, 320, "Morometii", 45.5f, '1');
 
     afisareCarte(c1);
     modificaPretCarte(&c1, 39.99f);
     afisareCarte(c1);
 
     dezalocareCarte(&c1);
-    afisareCarte(c1);
+    afisareCarte(c1);*/
+
+    int nrCarti = 4;
+    struct Carte* carti = (struct Carte*)malloc(sizeof(struct Carte) * nrCarti);
+
+    carti[0] = initializareCarte(1, 320, "Morometii", 45.5f, '1');
+    carti[1] = initializareCarte(2, 180, "Poezii", 22.0f, '2');
+    carti[2] = initializareCarte(3, 250, "Chimie Organica", 89.9f, '2');
+    carti[3] = initializareCarte(4, 140, "Retete rapide", 18.5f, '3');
+
+    printf("Vector initial:\n");
+    afisareVectorCarti(carti, nrCarti);
+
+
+    int nrPrimele = 2;
+    struct Carte* primele = copiazaPrimeleNCarti(carti, nrCarti, nrPrimele);
+    printf("\nPrimele carti:\n");
+    afisareVectorCarti(primele, nrPrimele);
+
+    struct Carte* cartiIeftine = NULL;
+    int nrCartiIeftine = 0;
+    copiazaCartiIeftine(carti, nrCarti, 25.0f, &cartiIeftine, &nrCartiIeftine);
+    printf("\nCarti ieftine:\n");
+    afisareVectorCarti(cartiIeftine, nrCartiIeftine);
+
+
+    dezalocareVectorCarti(&primele, &nrPrimele);
+    dezalocareVectorCarti(&cartiIeftine, &nrCartiIeftine);
+    dezalocareVectorCarti(&carti, &nrCarti);
 
     return 0;
 }
